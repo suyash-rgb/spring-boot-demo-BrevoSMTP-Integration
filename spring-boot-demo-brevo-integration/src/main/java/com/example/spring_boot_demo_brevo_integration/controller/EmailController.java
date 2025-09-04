d3d686a3d62b3fca45efbcbd5161151c5a297466
Suyash Baoney <suyashbaoney58@gmail.com>
2025-09-04 20:23:30 +0530

    HTML mailing endpoint added

--- spring-boot-demo-brevo-integration/src/main/java/com/example/spring_boot_demo_brevo_integration/controller/EmailController.java
+++ spring-boot-demo-brevo-integration/src/main/java/com/example/spring_boot_demo_brevo_integration/controller/EmailController.java
@@ -1,7 +1,9 @@
 package com.example.spring_boot_demo_brevo_integration.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,29 @@ public class EmailController {
      @Autowired
     private EmailService emailService;
 
+    @PostMapping("/send-text")
     //send a simple mail
-    public ResponseEntity<String> sendTextEmail(@RequestBody EmailRequest emailRequest){
-        
+    public ResponseEntity<String> sendTextEmail(@RequestBody EmailRequest request){
+        try{
+              emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+              return ResponseEntity.ok("Email sent successfully");
+               
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("Failed to send email" + e.getMessage());
+        }
+    }
+
+    @PostMapping("/send-html")
+    //send a html mail
+    public ResponseEntity<String> sendHtmlEmail(@RequestBody EmailRequest request){
+        try{
+              emailService.sendHtmlEmail(request.getTo(), request.getSubject(), request.getBody());
+              return ResponseEntity.ok("HTML Email sent successfully");
+               
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("Failed to send email" + e.getMessage());
+        }
     }
 }
