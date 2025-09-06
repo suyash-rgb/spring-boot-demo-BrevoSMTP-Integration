7534abccca1e79e75d6e12d607e857f306ed3dcc
Suyash Baoney <suyashbaoney58@gmail.com>
2025-09-07 00:03:57 +0530

    replaced sensitive info with sample values

--- spring-boot-demo-brevo-integration/src/main/java/com/example/spring_boot_demo_brevo_integration/controller/EmailController.java
+++ spring-boot-demo-brevo-integration/src/main/java/com/example/spring_boot_demo_brevo_integration/controller/EmailController.java
@@ -15,13 +15,15 @@ import com.example.spring_boot_demo_brevo_integration.service.EmailService;
 @RequestMapping("/api/email")
 public class EmailController {
 
-     @Autowired
+    @Autowired
     private EmailService emailService;
 
     @PostMapping("/send-text")
     //send a simple mail
     public ResponseEntity<String> sendTextEmail(@RequestBody EmailRequest request){
         try{
+              System.out.println("Request received: " + request);
+              
               emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
               return ResponseEntity.ok("Email sent successfully");
                
@@ -35,6 +37,7 @@ public class EmailController {
     //send a html mail
     public ResponseEntity<String> sendHtmlEmail(@RequestBody EmailRequest request){
         try{
+              System.out.println("Request received: " + request);
               emailService.sendHtmlEmail(request.getTo(), request.getSubject(), request.getBody());
               return ResponseEntity.ok("HTML Email sent successfully");
                
--- spring-boot-demo-brevo-integration/src/main/java/com/example/spring_boot_demo_brevo_integration/service/EmailService.java
+++ spring-boot-demo-brevo-integration/src/main/java/com/example/spring_boot_demo_brevo_integration/service/EmailService.java
@@ -3,7 +3,11 @@ package com.example.spring_boot_demo_brevo_integration.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -13,10 +17,20 @@ public class EmailService {
 
     public void sendEmail(String to, String subject, String body){
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("your_email");
+        message.setFrom("your_email@example.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
+        mailSender.send(message);
+    }
+
+    public void sendHtmlEmail(String to, String subject, String htmlContent) throws MessagingException{
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("your_email@example.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlContent, true);
         mailSender.send(message);
     }
 
